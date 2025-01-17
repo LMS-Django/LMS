4ce9a674f7479df0d81ab23db46fd5c37144f5d6
Nikita Sharnin <116027490+loymeding@users.noreply.github.com>
2025-01-17 21:06:46 +0400

    Add files via upload

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,17 +4,17 @@
 sleep 5
 
 # Проверяем и выполняем миграции только если они еще не применены
-if python manage.py migrate --check --noinput; then
+if python3 manage.py migrate --check --noinput; then
     echo "Миграции уже применены."
 else
     echo "Применяем миграции..."
-    python manage.py migrate --noinput
+    python3 manage.py migrate --noinput
 fi
 
 # Создаем суперпользователя, если он не существует
 echo "Создаем суперпользователя..."
-python create_superuser.py
+python3 create_superuser.py
 
 # Запускаем сервер
 echo "Запуск сервера..."
-exec python manage.py runserver 0.0.0.0:8000
+exec python3 manage.py runserver 0.0.0.0:8000
